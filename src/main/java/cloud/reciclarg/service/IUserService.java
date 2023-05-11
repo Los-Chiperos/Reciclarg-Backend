1fafa0f02e6a5f90d5f59592e68a16910870de32
Oscar Gonzalo Araya <gonxaraya991@gmail.com>
2023-05-11 10:24:35 -0300

    IUserService Comentarios
    Se agregó la explicación de los métodos declarados en la interfaz del servicio de usuarios.

--- src/main/java/cloud/reciclarg/service/IUserService.java
+++ src/main/java/cloud/reciclarg/service/IUserService.java
@@ -2,15 +2,19 @@ package cloud.reciclarg.service;
 
 import cloud.reciclarg.model.User;
 
+//Esta interfaz define un conjunto de métodos que deben ser implementados por cualquier clase que la implemente.
 public interface IUserService {
 
-        public User buscarUserByName(String username) throws Exception;
-        //ublic List<User> VerUsuarios();
+    //Este método busca un usuario por su nombre de usuario y devuelve un objeto User. Puede lanzar una excepción de tipo Exception.
+    User buscarUserByName(String username) throws Exception;
 
-        public void SaveUsuario(User user) throws Exception;
+    //Este método se utiliza para guardar un objeto User en alguna fuente de datos. Puede lanzar una excepción de tipo Exception.
+    void SaveUsuario(User user) throws Exception;
 
-        public void borrarUsuario(Long id) throws Exception;
+    //Este método se encarga de eliminar un usuario según su ID. Puede lanzar una excepción de tipo Exception.
+    void borrarUsuario(Long id) throws Exception;
 
-        public User buscarUserById(Long id) throws Exception;
-    
+    //Este método busca un usuario por su ID y devuelve un objeto User. Puede lanzar una excepción de tipo Exception.
+    User buscarUserById(Long id) throws Exception;
+
 }
